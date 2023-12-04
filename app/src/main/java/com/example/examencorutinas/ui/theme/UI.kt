335f2ca6b6ac6f5f2fd33cb404450356eff93892
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-04 13:19:32 +0100

    commit co lugar para as frases botons de V e F e o score, vai mal

--- app/src/main/java/com/example/examencorutinas/ui/theme/UI.kt
+++ app/src/main/java/com/example/examencorutinas/ui/theme/UI.kt
@@ -1,8 +1,6 @@
 package com.example.examencorutinas.ui.theme
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,9 +9,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.random.Random
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 
 data class Frase(var texto: String, var verdadero: Boolean)
 
@@ -21,6 +29,7 @@ var frases: MutableList<Frase> = mutableListOf()
 var fraseActual: MutableState<Frase> = mutableStateOf(Frase("-", true))
 var countdownValue: MutableState<Int> = mutableStateOf(20)
 var gameStarted: MutableState<Boolean> = mutableStateOf(false)
+var score: MutableState<Int> = mutableStateOf(0)
 
 @Composable
 fun FraseGame() {
@@ -34,6 +43,7 @@ fun FraseGame() {
                 if (!gameStarted.value) {
                     gameStarted.value = true
                     countdownValue.value = 20
+                    score.value = 0 // Reiniciar la puntuación al iniciar el juego
                     startCountdown()
                 }
             }
@@ -42,27 +52,57 @@ fun FraseGame() {
         }
 
         Text(text = "Countdown: ${countdownValue.value}")
+        val customH5Style = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp // Tamaño de fuente equivalente a h5
+        )
 
         BasicTextField(
             value = fraseActual.value.texto,
             onValueChange = { /* No op, read-only field */ },
             modifier = Modifier.padding(8.dp),
-            textStyle = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold)
+            textStyle = customH5Style
         )
+
+        Row(
+            modifier = Modifier.padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Button(
+                onClick = { checkAnswer(true) },
+                enabled = gameStarted.value
+            ) {
+                Text(text = "V")
+            }
+
+            Button(
+                onClick = { checkAnswer(false) },
+                enabled = gameStarted.value
+            ) {
+                Text(text = "F")
+            }
+        }
+
+        Text(text = "Score: ${score.value}")
     }
 }
 
 fun startCountdown() {
     CoroutineScope(Dispatchers.Default).launch {
         while (countdownValue.value > 0) {
-            withContext(Dispatchers.Main) {
-                countdownValue.value--
-                fraseActual.value = frases.random()
-            }
-            kotlinx.coroutines.delay(1000)
+            countdownValue.value--
+            fraseActual.value = frases.random()
+            delay(1000)
         }
         gameStarted.value = false
     }
+}
+
+fun checkAnswer(answer: Boolean) {
+    if (answer == fraseActual.value.verdadero) {
+        score.value++
+    }
+    fraseActual.value = frases.random() // Mostrar nueva frase aleatoria
 }
 
 @Preview
@@ -70,3 +110,6 @@ fun startCountdown() {
 fun PreviewFraseGame() {
     FraseGame()
 }
+
+
+
