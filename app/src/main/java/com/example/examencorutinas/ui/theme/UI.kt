51988d8a28e29368c9467df334d198e42e5c91de
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-12-05 13:34:50 +0100

    cambio o UI e poño as frases nos Datos.kt

--- app/src/main/java/com/example/examencorutinas/ui/theme/UI.kt
+++ app/src/main/java/com/example/examencorutinas/ui/theme/UI.kt
@@ -28,24 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 
 
-// Clase para almacenar una frase y su valor verdadero/falso
-data class Frase(var texto: String, var verdadero: Boolean)
 
-// Función para cargar una lista de frases
-fun cargarFrases(): MutableList<Frase> {
-    return mutableListOf(
-        Frase("el torneo de rugby cinco naciones, ahora es seis naciones", true),
-        Frase("en el cielo hay cinco estrellas", false),
-        Frase("el dia cinco de diciembre del 2023 es martes", true),
-        Frase("cinco más cinco son diez", true),
-        Frase("dos mas dos son cinco", false),
-        Frase("los elefantes tienen cinco patas", false),
-        Frase("las estaciones climáticas son cinco", false),
-        Frase("tenemos cinco dedos los humanos", true),
-        Frase("cinco días tiene la semana sin el Domingo y el Sábado", true),
-        Frase("una gallina pesa menos que cinco toneladas", true)
-    )
-}
 
 @Composable
 fun FraseGame() {
